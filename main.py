2759d5bae8deff4348a52248fc2ade3a5b92dfa4
jaemin6 <gumt555@gmail.com>
2025-12-24 16:14:49 +0900

    Franka 로봇 기본 설정 완료

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ simulation_app = SimulationApp({"headless": False})
 from omni.isaac.core import World
 from omni.isaac.franka import Franka
 
+# 월드 생성
 world = World(stage_units_in_meters=1.0)
 
+# Franka 로봇 추가
 franka = Franka(
     prim_path="/World/Franka",
     name="franka"
 )
 
+# 초기화
 world.reset()
 
-print("Simulation Start")
+print("Franka loaded")
 
+# 시뮬레이션 루프
 while simulation_app.is_running():
     world.step(render=True)
 
