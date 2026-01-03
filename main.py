23ff2f630f129e15036b9bda244e406140e4dec8
jaemin6 <gumt555@gmail.com>
2026-01-03 15:28:18 +0900

    SimulationApp 생성 후 pxr / omni / isaac 관련 모듈을 import

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from omni.isaac.kit import SimulationApp
+from isaacsim.simulation_app import SimulationApp
+# 1. SimulationApp 먼저 생성
+simulation_app = SimulationApp({"headless": False})
+# 2. 그 다음 Isaac 관련 모듈 import
 from sim.world import SimulationWorld
 from sim.robot import FrankaRobot
-
-simulation_app = SimulationApp({"headless": False})
 
 sim_world = SimulationWorld()
 world = sim_world.get_world()
