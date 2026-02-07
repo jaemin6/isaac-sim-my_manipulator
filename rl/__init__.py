241d975d903043ba57d78cef492da44df777c411
jaemin6 <gumt555@gmail.com>
2026-02-07 17:37:18 +0900

    rl 패키지 진입점 설정 및 외부 노출 인터페이스(API) 정의

---
+++ rl/__init__.py
@@ -0,0 +1,57 @@
+"""
+강화학습 (Reinforcement Learning) 모듈
+
+이 모듈은 암호화폐 트레이딩을 위한 PPO 기반 강화학습을 제공합니다.
+"""
+
+from .environment import TradingEnvironment, MultiAssetTradingEnvironment
+from .networks import ActorCritic, RecurrentActorCritic, CNNActorCritic
+from .ppo_agent import PPOAgent
+from .replay_buffer import RolloutBuffer, EpisodeBuffer, PrioritizedReplayBuffer
+from .config import (
+    RLConfig,
+    EnvironmentConfig,
+    NetworkConfig,
+    PPOConfig,
+    TrainingConfig,
+    EvaluationConfig,
+    get_config,
+    DEFAULT_CONFIG,
+    CONSERVATIVE_CONFIG,
+    AGGRESSIVE_CONFIG,
+    RECURRENT_CONFIG
+)
+
+__all__ = [
+    # Environment
+    'TradingEnvironment',
+    'MultiAssetTradingEnvironment',
+    
+    # Networks
+    'ActorCritic',
+    'RecurrentActorCritic',
+    'CNNActorCritic',
+    
+    # Agent
+    'PPOAgent',
+    
+    # Replay Buffer
+    'RolloutBuffer',
+    'EpisodeBuffer',
+    'PrioritizedReplayBuffer',
+    
+    # Config
+    'RLConfig',
+    'EnvironmentConfig',
+    'NetworkConfig',
+    'PPOConfig',
+    'TrainingConfig',
+    'EvaluationConfig',
+    'get_config',
+    'DEFAULT_CONFIG',
+    'CONSERVATIVE_CONFIG',
+    'AGGRESSIVE_CONFIG',
+    'RECURRENT_CONFIG'
+]
+
+__version__ = '0.1.0'
